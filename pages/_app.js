1a43652e03c22d1aca809df1d569de02c97e2245
Kjddubrioq142 <110691932+Kjddubrioq142@users.noreply.github.com>
2023-06-12 04:25:13 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import '@styles/globals.css'
 
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
-  <Script src="/script.js" />
 }
 
 export default Application

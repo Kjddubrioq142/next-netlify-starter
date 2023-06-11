5e0c1db9a61798e7ef372675e27371e0269e9e93
Kjddubrioq142 <110691932+Kjddubrioq142@users.noreply.github.com>
2023-06-12 04:33:50 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,9 +17,9 @@ export default function Home() {
         </p>
       </main>
       <Script
-        document.onvisibilitychange = function() {
+        const handleVisibilityChange = () => {
           if (document.visibilityState === 'hidden') {
-            alert("test");
+            alert('test');
           }
         };
       />
